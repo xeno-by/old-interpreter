06ebfe965b8511f66a611eadb4d2c38b203a995d
Eugene Burmako <xeno.by@gmail.com>
2014-03-18 20:09:43 +0100

    interprets matches

--- interpreter/src/main/scala/scala/reflect/interpreter/internal/Engine.scala
+++ interpreter/src/main/scala/scala/reflect/interpreter/internal/Engine.scala
@@ -40,7 +40,7 @@ abstract class Engine extends InterpreterRequires with Definitions with Errors {
     // case q"(..$exprs)"                     => never going to happen, because parser desugars these trees into applications
     case q"{ ..$stats }"                      => evalBlock(stats, env)
     case q"if ($cond) $then1 else $else1"     => evalIf(cond, then1, else1, env)
-    case q"$scrut match { case ..$cases }"    => ???
+    case q"$scrut match { case ..$cases }"    => evalMatch(scrut, cases, env)
     case q"try $expr catch { case ..$cases } finally $finally1" => ???
     case q"(..$params) => $body"              => Value.function(params, body, env)
     // case q"{ case ..$cases }"              => never going to happen, because typer desugars these trees into anonymous class instantiations
@@ -171,6 +171,93 @@ abstract class Engine extends InterpreterRequires with Definitions with Errors {
   def evalIf(cond: Tree, then1: Tree, else1: Tree, env: Env): Result = {
     val Result(vcond, env1) = eval(cond, env)
     vcond.branch(eval(then1, env1), eval(else1, env1))
+  }
+
+  def evalMatch(scrut: Tree, cases: List[CaseDef], env: Env): Result = {
+    // TODO: semantics of evalPattern is just a sketch
+    // it should be validated against the spec and the code that's emitted by the pattern matcher
+    // TODO: also support name-based pattern matching
+    // https://github.com/scala/scala/pull/2848
+    def succeed(menv: Env, renv: Env) = eval(q"true", renv)
+    def fail(menv: Env, renv: Env) = eval(q"false", renv)
+    def evalPattern(vscrut: Value, pat: Tree, menv: Env, renv: Env): Result = {
+      def checkCond(cond: Tree, onSuccess: (Env, Env) => Result = succeed, onFailure: (Env, Env) => Result = fail) = {
+        val Result(vcond, menv1) = eval(cond, menv)
+        val renv1 = renv.extend(menv1.heap)
+        vcond.branch(onSuccess(menv1, renv1), onFailure(menv1, renv1))
+      }
+      def checkPat(pat: Tree, onSuccess: (Env, Env) => Result = succeed, onFailure: (Env, Env) => Result = fail) = {
+        val Result(vpat, renv1) = evalPattern(vscrut, pat, menv, renv)
+        val menv1 = menv.extend(renv1.heap)
+        vpat.branch(onSuccess(menv1, renv1), onFailure(menv1, renv1))
+      }
+      def checkEval(expr: Tree, cond: Value => Tree, result: Value => Tree, onSuccess: (Value, Env, Env) => Result) = {
+        val Result(vexpr, menv1) = eval(expr, menv)
+        val Result(vcond, menv2) = eval(cond(vexpr), menv1)
+        def cont = {
+          val Result(vresult, menv3) = eval(result(vexpr), menv2)
+          onSuccess(vresult, menv3, renv.extend(menv3.heap))
+        }
+        vcond.branch(cont, fail(menv2, renv.extend(menv2.heap)))
+      }
+      pat match {
+        case Ident(termNames.WILDCARD) =>
+          succeed(menv, renv)
+        case Typed(_, tpt) =>
+          checkCond(q"$vscrut.${Any_isInstanceOf}[$tpt]()")
+        case Bind(name, pat) =>
+          checkPat(pat, onSuccess = (menv1, renv1) => succeed(menv1, renv1.extend(pat.symbol, vscrut)))
+        case pat: Literal =>
+          checkCond(q"$vscrut.${Any_equals}($pat)")
+        case pat: RefTree =>
+          checkCond(q"$vscrut.${Object_eq}($pat)")
+        case Alternative(alt1 :: alt2 :: rest) =>
+          val fallback = if (rest.isEmpty) alt2 else Alternative(alt2 :: rest)
+          checkPat(alt1, onFailure = (menv1, renv1) => evalPattern(vscrut, fallback, menv1, renv1))
+        case Apply(tpt, pats) => // can only happen when tpt is a case class
+          def cont(menv1: Env, renv1: Env) = {
+            val fields = tpt.tpe.decls.sorted.filter(sym => sym.isMethod && sym.asMethod.isCaseAccessor)
+            val Results(vfields, matchEnv2) = eval(fields.map(f => q"$scrut.$f"), menv1)
+            val renv1 = renv.extend(matchEnv2.heap)
+            evalPatterns(vfields, pats, matchEnv2, renv1)
+          }
+          checkCond(q"$vscrut.${Any_isInstanceOf}[$tpt]()", onSuccess = cont)
+        case UnApply(Apply(unapply, List(dummy)), Nil) => // this is what other extractors are translated to
+          checkCond(q"$unapply($vscrut)")
+        case UnApply(Apply(unapply, List(dummy)), pats) =>
+          def cont(result: Value, menv1: Env, renv1: Env) = {
+            val fields = TupleClass(pats.length).info.decls.sorted.filter(sym => sym.isMethod && sym.asMethod.isCaseAccessor)
+            val Results(vfields, menv2) = {
+              if (pats.length == 1) Results(result :: Nil, menv1)
+              else eval(fields.map(f => q"$scrut.$f"), menv1)
+            }
+            evalPatterns(vfields, pats, menv2, renv1.extend(menv2.heap))
+          }
+          checkEval(q"$unapply($vscrut)", vexpr => q"$vexpr.${Option_isDefined}", vexpr => q"$vexpr.${Option_get}", onSuccess = cont)
+      }
+    }
+    def evalPatterns(vscruts: List[Value], pats: List[Tree], menv: Env, renv: Env): Result = (vscruts, pats) match {
+      case (vscruts, Star(pat) :: Nil) =>
+        val Result(varrayOfScruts, menv1) = (??? : Any) // TODO: create a vararg array from values represented by vscruts
+        succeed(menv1, renv.extend(menv1.heap).extend(pat.symbol, varrayOfScruts))
+      case (vscrut :: vrest, pat :: patrest) =>
+        val Result(result, renv1) = evalPattern(vscrut, pat, menv, renv)
+        result.branch(evalPatterns(vrest, patrest, menv.extend(renv1.heap), renv1), fail(menv, renv1))
+      case (Nil, Nil) =>
+        succeed(menv, renv)
+    }
+    def loop(vscrut: Value, cases: List[CaseDef], env: Env): Result = cases match {
+      case CaseDef(pat, guard, body) :: rest =>
+        val Result(vpat, env1) = evalPattern(vscrut, pat, env, env)
+        val Result(vguard, env2) = eval(guard.orElse(q"true"), env1)
+        vguard.branch(eval(body, env2), loop(vscrut, rest, env2))
+      case _ =>
+        val Result(exn, env1) = Value.instantiate(typeOf[MatchError], env)
+        val Result(exn1, env2) = exn.apply(List(vscrut), env1)
+        ??? // TODO: throw exn1 in env2
+    }
+    val Result(vscrut, env1) = eval(scrut, env)
+    loop(vscrut, cases, env1)
   }
 
   final case class Scope() // TODO: figure out how to combine both lexical scope (locals and globals) and stack frames
