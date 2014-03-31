44d7b5ed1b54d01ae6a9bb919489225f38697a4e
Eugene Burmako <xeno.by@gmail.com>
2014-03-31 15:14:26 +0200

    moves magic method definitions to Definitions.scala

--- interpreter/src/main/scala/scala/reflect/interpreter/internal/Emulators.scala
+++ interpreter/src/main/scala/scala/reflect/interpreter/internal/Emulators.scala
@@ -11,21 +11,13 @@ trait Emulators {
   }
 
   trait MagicMethodEmulator extends PrimitiveEmulator {
-
-    val INT_PLUS_FLOAT = selectMethod[Int, Float]("$plus")
-    val INT_PLUS_INT = selectMethod[Int, Int]("$plus")
-    val INT_MINUS_INT = selectMethod[Int, Int]("$minus")
-    val INT_LESS_INT = selectMethod[Int, Int]("$less")
-    val INT_GT_INT = selectMethod[Int, Int]("$greater")
-    val INT_EQEQ_INT = selectMethod[Int, Int]("$eq$eq")
-
-    def selectCallable(value: Value, sym: Symbol, env: Env): EmulatedCallableValue = {
-      val f = (args: List[Value], env: Env) => {
-        def binOp[T, K](f: (T, K) => Any): Result = {
+    def selectCallable(value: Value, sym: Symbol, env: Env) = {
+      new EmulatedCallableValue((args: List[Value], env: Env) => {
+        def binOp[T1, T2](f: (T1, T2) => Any): Result = {
           val (jvalue, env1) = value.reify(env)
           val arg :: Nil = args
           val (jarg, env2) = arg.reify(env1)
-          val jresult = f(jvalue.asInstanceOf[T], jarg.asInstanceOf[K])
+          val jresult = f(jvalue.asInstanceOf[T1], jarg.asInstanceOf[T2])
           Value.reflect(jresult, env2)
         }
         sym match {
@@ -37,16 +29,7 @@ trait Emulators {
           case INT_PLUS_FLOAT   => binOp[Int, Float](_ + _)
           case other            => UnsupportedEmulation(sym)
         }
-      }
-
-      new EmulatedCallableValue(f)
-    }
-
-    def selectMethod[T: TypeTag, K: TypeTag](methodName: String):Symbol = {
-      u.symbolOf[T].info.member(TermName(methodName)).alternatives.find(m =>
-        m.asMethod.paramLists.head.head.typeSignature == u.typeOf[K]).get
+      })
     }
   }
-
 }
-
