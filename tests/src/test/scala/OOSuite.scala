482f5fe202f125ebfc48ed290ace52bf20fef1a7
dedoz <d3d0zz@gmail.com>
2014-05-02 15:47:50 +0400

    add object tests
    - add self type test
    - add self type with multiple injections test
    - add class closure test
    - add class inside module test
    - add class inside function test
    - add ctor default parameters test
    - add empty class test
    - add lazy field tests

--- tests/src/test/scala/OOSuite.scala
+++ tests/src/test/scala/OOSuite.scala
@@ -2,6 +2,10 @@ import org.scalatest.FunSuite
 
 class OOSuite extends FunSuite {
 
+  test("empty object") {
+    assert(ctfe { class A; new A; 42 } == 42)
+  }
+
   test("trait method lookup simple") {
     assert(ctfe { trait A { def f() = 42 }; object B extends A; B.f() } == 42)
   }
@@ -30,6 +34,28 @@ class OOSuite extends FunSuite {
       val a = new A
       a.v = 42
       a.v
+    } == 42)
+  }
+
+  test("lazy fields") {
+    assert(ctfe {
+      class A {
+        var v = 100
+        lazy val a = v
+        v = 42
+      }
+      (new A).a
+    } == 42)
+  }
+
+  test("field references in initialization") {
+    assert(ctfe {
+      class A {
+        val a = 100
+        val b = a
+        val c = b - a + 42
+      }
+      (new A).c
     } == 42)
   }
 
@@ -79,11 +105,23 @@ class OOSuite extends FunSuite {
     assert(ctfe { class A(val v: Int); new A(42).v } == 42 )
   }
 
+  test("constructor default parameters") {
+    assert(ctfe { class A(val v: Int, val z: Int = 40){def f = v+z}; new A(2).f } == 42)
+  }
+
   test("constructor initialization order") {
     assert(ctfe { class A(var v: Int){v = 42}; new A(999).v } == 42)
   }
 
-  test("multiple parameter list cons") {
+  test("ctor field mapping") {
+    assert(ctfe {
+      class A(x: Int){def f = x}
+      class B(val x: Int)
+      new A(40).f + new B(2).x
+    } == 42)
+  }
+
+  test("multiple parameter list ctor") {
     assert(ctfe { class A(a:Int)(b:Int){def f = a+b}; new A(40)(2).f } == 42)
   }
 
@@ -95,7 +133,7 @@ class OOSuite extends FunSuite {
     } == 42)
   }
 
-  test("lazy field initialization") {
+  test("lazy field initialization in constructor") {
     assert(ctfe {
       abstract class A { val x: Int; val y = x}
       class B extends A {lazy val x = 42}
@@ -103,11 +141,69 @@ class OOSuite extends FunSuite {
     } == 42)
   }
 
-  test("anonymous class with") {
+  test("anonymous class with early definitions") {
     assert(ctfe {
       trait F {val x: Int}
       val v = new {val x = 42} with F
       v.x
+    } == 42)
+  }
+
+  test("self type") {
+    assert(ctfe {
+      trait A {def f = 999}
+      trait B extends A {override def f = 22}
+      class C {
+        self: A =>
+        def g = 20 + f
+      }
+      (new C with B).g
+    } == 42)
+  }
+
+  test("self type with multiple injections") {
+    assert(ctfe {
+      trait A {def f = 999}
+      trait B extends A {override def f = 22}
+      trait K {def k = 888}
+      trait M extends K {override def k = 19}
+      class C {
+        self: A with K=>
+        def g = 1 + f + k
+      }
+      (new C with B with M).g
+    } == 42)
+  }
+
+  test("class closure") {
+    assert(ctfe {
+      class A(a: Int) {
+        class Inner {def f(v: Int) = a + v}
+        def g = (new Inner).f(22)
+      }
+      new A(20).g
+    } == 42)
+  }
+
+  test("class inside a function") {
+    assert(ctfe {
+      class B {val a = 20}
+      def f(a: Int) = {
+        class A(val v: Int) extends B
+        new A(a).v + new A(a).a
+      }
+      f(22)
+    } == 42)
+  }
+
+  test("class inside a module") {
+    assert(ctfe {
+      object Obj {
+        val v = 40
+        class A{def f = v + 2}
+        def f = (new A).f
+      }
+      Obj.f
     } == 42)
   }
 }
